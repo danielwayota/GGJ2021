422657c521e9ad55f168d8b02ab920fc564c16c5
Daniel Wayota <danielglez1990@gmail.com>
2021-01-30 13:00:36 +0000

    Basic movement and proto-sprites

---
+++ Assets/Scripts/Tana.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tana : MonoBehaviour
+{
+    public float speed = 2f;
+
+    private Rigidbody2D body;
+
+    void Start()
+    {
+        this.body = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        var mov = (new Vector2(h, v)).normalized;
+
+        this.body.velocity = mov * this.speed;
+    }
+}
